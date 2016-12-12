b2a6950555341b8277397fb1e06155ed17093ee6
Krystal Campioni <krystal.designer@gmail.com>
2016-12-11 23:57:36 -0200

    include simple bar chart to visualize sea level difference

--- app/js/directives.js
+++ app/js/directives.js
@@ -12,6 +12,17 @@ angular.module('weatherApp.directives', [])
         scope.isNine = function(dt_txt){
           return _.includes(dt_txt, '09:00')
         };
+
+        scope.calculateSeaRise = function(sea, ground){
+          if (sea - ground > 0) {
+            var positiveNumber = '+' + (sea-ground).toString();
+            return positiveNumber
+          }
+          else {
+            return sea - ground
+          }
+
+        };
       }
     }
   }])
